030906f6c9c6bfa8a8befc88ff1fe7761caa76c4
Csaba <56678429+CsabaVK@users.noreply.github.com>
2019-11-29 09:38:14 +0100

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,8 @@
+create TABLE pokemonok(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nev varchar (100) not null,
+    tipus varchar (100) not null);
+	
+INSERT INTO pokemonok (nev, tipus) VALUES
+('pikachu', 'thunder'),
+('charmender' , 'fire');
---
+++ readme.md
@@ -0,0 +1,11 @@
+Tools --> NuGet Package Manager
+Browse --> 'MySql.Data'
+Install
+
+STRING CONNECTION
+https://www.connectionstrings.com/mysql/
+
+DATABASE
+winsql.vereb.dc
+diak71
+F**xBh
